68751a90b26f85560a1e72c53db9863b09878946
milanbot192 <milansudheera81@gmail.com>
2024-12-24 14:39:02 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,7 +4,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=bzAEXaBS#rpmhgi8PvtIv4tInnqPKmMK5hlMC360qmk8OLZG-1xk' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=qvp1iahD#1yUUmaJc3rUToWlPODmuI19W8rbhqqtxzk91pkUZcAM' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 ALIVE_MSG: process.env.ALIVE_MSG === undefined ? "i.am alive now"
 
